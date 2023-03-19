54b0a91949eed8b73718073c6d538fd28c6c831a
jellejurre <jelle@jilles.com>
2023-03-19 23:50:21 +0100

    Fix layer ft test

--- Assets/jellejurre/Benchmarker/Scripts/Util/AnimationAdder.cs
+++ Assets/jellejurre/Benchmarker/Scripts/Util/AnimationAdder.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class AnimationAdder : MonoBehaviour
 {
-	public AnimationClip clip;
 	public AnimatorController controller;
+	private int index;
 
 	public void DoThing()
 	{
 		AnimatorControllerLayer[] layers = controller.layers;
+		index = 0;
 		for (var i = 0; i < layers.Length; i++)
 		{
 			AnimatorControllerLayer l = layers[i];
 			ChildAnimatorState[] states = l.stateMachine.states;
 			for (var i1 = 0; i1 < states.Length; i1++)
 			{
-				states[i1].state.motion = AssignStates(states[i1].state.motion, clip);
+				states[i1].state.motion = AssignStates(states[i1].state.motion);
 			}
 
 			l.stateMachine.states = states;
@@ -26,20 +27,22 @@ public class AnimationAdder : MonoBehaviour
 		controller.layers = layers;
 	}
 
-	public Motion AssignStates(Motion motion, AnimationClip clip)
+	public Motion AssignStates(Motion motion)
 	{
 		if (motion is BlendTree tree)
 		{
 			var treeChildren = tree.children;
 			for (var i = 0; i < treeChildren.Length; i++)
 			{
-				treeChildren[i].motion = AssignStates(treeChildren[i].motion, clip);
+				treeChildren[i].motion = AssignStates(treeChildren[i].motion);
 			}
 
 			tree.children = treeChildren;
 			return tree;
 		}
-		return clip;
+
+		index++;
+		return AnimationHelper.GetOrCreateTwoStateToggle("test"+index, index)[0];
 	}
 }
 
