c039f767175a679718efaa9443c34c9a76c97103
jellejurre <jelle@jilles.com>
2023-03-22 12:57:58 +0100

    Perform in vrc contact tests

--- Assets/jellejurre/Benchmarker/Scripts/Util/SetupTestAvatars.cs
+++ Assets/jellejurre/Benchmarker/Scripts/Util/SetupTestAvatars.cs
@@ -11,25 +11,59 @@ public class SetupTestAvatars : MonoBehaviour
 {
 	public GameObject prefab;
 	public int amount;
+	public bool senders;
+	public bool receivers;
+	public AnimationClip rotate;
 
 	public void SetupProximity()
 	{
 		GameObject gameObject = Instantiate(prefab);
 		Animator animator = gameObject.GetOrAddComponent<Animator>();
-		for (int i = 0; i < amount; i++)
+		if (receivers)
 		{
-			int width = (int)Math.Sqrt(amount);
-			int x = i % width;
-			int y = (int)(i - x) / width;
-			GameObject toggleObject = new GameObject("test"+ i.ToString());
-			toggleObject.transform.parent = gameObject.transform;
-			ContactReceiver constraint = toggleObject.AddComponent<VRCContactReceiver>();
-			constraint.position = new Vector3((x - width/2)/10f, (y-width/2)/10f, 0);
-			constraint.radius = 0.1f;
-			constraint.parameter = i.ToString();
-			constraint.collisionTags = new List<string>(){"Finger"};
+			for (int i = 0; i < amount; i++)
+			{
+				int width = (int)Math.Sqrt(amount);
+				int x = i % width;
+				int y = (int)(i - x) / width;
+				GameObject toggleObject = new GameObject("test" + i.ToString());
+				toggleObject.transform.parent = gameObject.transform;
+				VRCContactReceiver constraint = toggleObject.AddComponent<VRCContactReceiver>();
+				constraint.position = new Vector3((x - width / 2) / 10f, (y - width / 2) / 10f, 0);
+				constraint.radius = 0.1f;
+				constraint.shapeType = ContactBase.ShapeType.Capsule;
+				constraint.parameter = i.ToString();
+				constraint.collisionTags = new List<string>() { "Finger" };
+			}
 		}
-		AnimatorController controller = AnimatorHelpers.SetupTwoToggles(amount);
+
+		if (senders)
+		{
+			for (int i = 0; i < amount; i++)
+			{
+				GameObject toggleObject = new GameObject("tast" + i.ToString());
+				toggleObject.transform.parent = gameObject.transform;
+				ContactSender sender = toggleObject.AddComponent<VRCContactSender>();
+				sender.position = new Vector3(0, 0, i + 10);
+				sender.collisionTags = new List<string>() { "Finger" };
+			}
+		}
+
+		AnimatorController controller = new AnimatorController();
+		AnimatorHelpers.AddParameters(controller, amount);
+		AnimatorControllerLayer layer = new AnimatorControllerLayer();
+		layer.name = "rotate";
+		layer.defaultWeight = 1;
+		AnimatorStateMachine stateMachine = new AnimatorStateMachine();
+		AnimatorState state = new AnimatorState();
+		AnimationClip offAnim = rotate;
+		state.motion = offAnim;
+		stateMachine.AddState(state, Vector3.one);
+		layer.stateMachine = stateMachine;
+		AnimatorControllerLayer[] layers = new []{layer};
+		controller.layers = layers;
+		AssetDatabase.CreateAsset(controller, $"Assets/{amount}.controller");
+		AnimatorHelpers.SerializeController(controller);
 		animator.runtimeAnimatorController = controller;
 		animator.cullingMode = AnimatorCullingMode.AlwaysAnimate;
 	}
